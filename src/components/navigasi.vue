bcfd9b8338af566d10c60a9e4665df7c8d32a1c7
resipb <93574764+resipb@users.noreply.github.com>
2022-03-07 12:39:02 +0700

    Update navigasi.vue

--- src/components/navigasi.vue
+++ src/components/navigasi.vue
@@ -3,7 +3,6 @@
     <nav class="nav navbar-light" style="background-color: #e3f2fd">
       <router-link to="/" class="nav-link">Home</router-link>
       <router-link to="/Products" class="nav-link">Product</router-link>
-      <router-link to="/about" class="nav-link">About</router-link>
       <router-link to="/contact" class="nav-link">Contact</router-link>
     </nav>
   </div>
