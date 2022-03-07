76351d896f6336930e705a0b91fb5da2a08fd5a7
resipb <93574764+resipb@users.noreply.github.com>
2022-03-07 12:32:51 +0700

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -11,11 +11,6 @@ const routes = [
     component: Home,
   },
   {
-    path: "/about",
-    name: "About",
-    component: () => import(/* webpackChunkName: "about" */ "../views/About.vue"),
-  },
-  {
     path: "/Products",
     name: "Products",
     component: () => import(/* webpackChunkName: "products" */ "../views/Products.vue"),
